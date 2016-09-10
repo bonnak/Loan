827006f9ab1b726fa6d5cf8c3e542d2223dd4f33
cuoool <chea.bonnak@gmail.com>
2016-09-10 20:43:26 +0700

    Remove user profile

--- resources/assets/js/components/Sidebar.vue
+++ resources/assets/js/components/Sidebar.vue
@@ -6,25 +6,6 @@
 	            <a href="index.html">Joli Admin</a>
 	            <a href="#" class="x-navigation-control"></a>
 	        </li>
-	        <li class="xn-profile">
-	            <a href="#" class="profile-mini">
-	                <img src="/joli/assets/images/users/avatar.jpg" alt="John Doe"/>
-	            </a>
-	            <div class="profile">
-	                <div class="profile-image">
-	                    <img src="/joli/assets/images/users/avatar.jpg" alt="John Doe"/>
-	                </div>
-	                <div class="profile-data">
-	                    <div class="profile-data-name">John Doe</div>
-	                    <div class="profile-data-title">Web Developer/Designer</div>
-	                    <div><a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a></div>
-	                </div>
-	                <div class="profile-controls">
-	                    <a href="pages-profile.html" class="profile-control-left"><span class="fa fa-info"></span></a>
-	                    <a href="pages-messages.html" class="profile-control-right"><span class="fa fa-envelope"></span></a>
-	                </div>
-	            </div>                                                                        
-	        </li>
 	        <li class="xn-title">Navigation</li>
 	        <li class="active">
 	            <a v-link="{ path: '/' }"><span class="fa fa-desktop"></span> <span class="xn-text">Dashboard</span></a>                        
