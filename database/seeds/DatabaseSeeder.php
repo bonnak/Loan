9e051c67ad33cfd299c6a05d9326355979d9066b
cuoool <chea.bonnak@gmail.com>
2016-09-11 16:22:57 +0700

    Add Province seeder to Database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(FruitsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(ProvincesTableSeeder::class);
     }
 }
