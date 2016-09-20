54f6624fc37d0a378024f67bed187f19160d8b60
cuoool <chea.bonnak@gmail.com>
2016-09-20 23:14:54 +0700

    Test user can insert or update province

--- tests/ProvinceTest.php
+++ tests/ProvinceTest.php
@@ -25,4 +25,41 @@ class ProvinceTest extends TestCase
        ]
      ]);
   }
+
+  /** @test */
+  public function it_store_new_province()
+  {
+    $user = factory(App\User::class)->create(['password' => '12345678']);
+
+    $form_data = [
+      'code' => 'KRA',
+      'name_en' => 'Kratie',
+      'name_kh' => 'ក្រចេះ', 
+    ];
+
+    $this->post('/api/province/store', $form_data, $this->headers($user))
+        ->seeInDatabase('provinces', $form_data);
+  }
+
+  /** @test */
+  public function it_update_province()
+  {
+    $user = factory(App\User::class)->create(['password' => '12345678']);
+
+    $form_data = [
+      'code' => 'PNP',
+      'name_en' => 'Phnom Penh',
+      'name_kh' => 'ភ្នំពេញ',
+    ];
+
+    $province = factory(App\Province::class)->create($form_data);
+    $this->seeInDatabase('provinces', $form_data);
+
+    $province->code = 'PNP1';
+    $province->name_en = 'Phnom Penh 1';
+    $province->name_kh = 'ភ្នំពេញ 1';
+
+    $this->put('/api/province/update', $province->toArray(), $this->headers($user))
+        ->seeInDatabase('provinces', $province->toArray());
+  }
 }
