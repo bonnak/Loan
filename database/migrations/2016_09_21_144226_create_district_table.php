ef561e30f859397c1e028a022e8f067823308c0d
cuoool <chea.bonnak@gmail.com>
2016-09-21 23:13:23 +0700

    Create district migration

---
+++ database/migrations/2016_09_21_144226_create_district_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('province_id')->unsigned();
+            $table->string('name_kh')->nullable();
+            $table->string('name_en')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('districts');
+    }
+}
